f0a3829c529fe6a91c9ba3afebd396caa38a2da5
Prashant Kumar <prashantkumar2036@gmail.com>
2025-11-13 21:47:43 +0530

    all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {ClerkProvider} from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
+import UserSync from "@/components/UserSync";
+import TanStackProvider from "@/components/Providers/TanStackProviders";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,8 +17,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Dental Assistant - AI Powered Dental Assistant",
-  description: "Get assistant by the AI powered tool Available for 24X7",
+  title: "DentWise - AI Powered Dental Assistant",
+  description:
+    "Get instant dental advice through voice calls with our AI assistant. Avaiable 24/7.",
 };
 
 export default function RootLayout({
@@ -24,24 +28,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-    appearance={{
-      variables:{
-        colorPrimary:"#e78a53",
-        colorBackground:"#f3f4f6",
-        colorText:"#111827",
-        colorTextSecondary:"#6b7280",
-        colorInputForeground:"#f3f4f6"
-      }
-    }}
-    >
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+    <TanStackProvider>
+      <ClerkProvider
+        appearance={{
+          variables: {
+            colorPrimary: "#e78a53",
+            colorBackground: "#f3f4f6",
+            colorText: "#111827",
+            colorTextSecondary: "#6b7280",
+            colorInputBackground: "#f3f4f6",
+          },
+        }}
       >
-        {children}
-      </body>
-    </html>
-    </ClerkProvider>
+        <html lang="en">
+          <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
+            {/* this is done in the home page component */}
+            {/* <UserSync /> */}
+            <Toaster />
+            {children}
+          </body>
+        </html>
+      </ClerkProvider>
+    </TanStackProvider>
   );
 }
