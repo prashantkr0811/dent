9568f202fb64dc2cf0fa747c89601c87990b6fed
Prashant Kumar <153224821+prashantkr0811@users.noreply.github.com>
2025-11-17 00:58:12 +0530

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import UserSync from "@/components/UserSync";
-import TanStackProvider from "@/components/providers/TanStackProvider";
+import TanStackProvider from "@/components/providers/TanStackProvider"
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
