9a6eb8108c5a98911f6f544806e0feac583cfaa6
Prashant Kumar <prashantkumar2036@gmail.com>
2025-11-16 06:22:23 +0530

    f

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
+// import resend from "@/lib/resend";
+// import type { NextConfig } from "next";
+// import { env } from "process";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "avatar.iran.liara.run",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "img.clerk.com",
+//       },
+//       env:{
+//         RESEND_API_KEY: process.env.RESEND_API_KEY,
+//       },
+//     ],
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
