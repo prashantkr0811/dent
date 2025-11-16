dfd03ee213b38aed07ff338bbddd44fa2db4a806
Prashant Kumar <153224821+prashantkr0811@users.noreply.github.com>
2025-11-16 22:51:08 +0530

    Update middleware.tsx

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,3 +1,25 @@
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isProtectedRoute = createRouteMatcher([
+//   "/dashboard(.*)",
+//   "/admin(.*)",
+//   "/appointments(.*)",
+//   "/voice(.*)",
+//   "/pro(.*)",
+// ]);
+
+// export default clerkMiddleware(async (auth, req) => {
+//   if (isProtectedRoute(req)) {
+//     await auth.protect();
+//   }
+// });
+
+// export const config = {
+//   matcher: [
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     "/(api|trpc)(.*)",
+//   ],
+// };
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
