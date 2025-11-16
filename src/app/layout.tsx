8494183479c4cc1a437c2b6c0c5e2ff19f6477e1
Prashant Kumar <153224821+prashantkr0811@users.noreply.github.com>
2025-11-17 01:39:55 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import UserSync from "@/components/UserSync";
-import TanStackProvider from "@/components/providers/TanStackProvider"
+import TanStackProvider from "@/components/Providers/TanStackProviders";
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
@@ -22,34 +23,42 @@ export const metadata: Metadata = {
     "Get instant dental advice through voice calls with our AI assistant. Avaiable 24/7.",
 };
 
+const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error("Missing NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY");
+}
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <TanStackProvider>
-      <ClerkProvider
-         publishableKey={publishableKey}
-        appearance={{
-          variables: {
-            colorPrimary: "#e78a53",
-            colorBackground: "#f3f4f6",
-            colorText: "#111827",
-            colorTextSecondary: "#6b7280",
-            colorInputBackground: "#f3f4f6",
-          },
-        }}
-      >
-        <html lang="en">
-          <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
-            {/* this is done in the home page component */}
-            {/* <UserSync /> */}
+    <ClerkProvider
+      publishableKey={publishableKey}
+      appearance={{
+        variables: {
+          colorPrimary: "#e78a53",
+          colorBackground: "#f3f4f6",
+          colorText: "#111827",
+          colorTextSecondary: "#6b7280",
+          colorInputBackground: "#f3f4f6",
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          suppressHydrationWarning
+          className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        >
+          <TanStackProvider>
+            <UserSync />
             <Toaster />
             {children}
-          </body>
-        </html>
-      </ClerkProvider>
-    </TanStackProvider>
+          </TanStackProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
