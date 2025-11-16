8e3df6995f388e343cee010a57234f88edf07065
Prashant Kumar <153224821+prashantkr0811@users.noreply.github.com>
2025-11-17 00:01:50 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,26 +28,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <TanStackProvider>
-      <ClerkProvider
-        appearance={{
-          variables: {
-            colorPrimary: "#e78a53",
-            colorBackground: "#f3f4f6",
-            colorText: "#111827",
-            colorTextSecondary: "#6b7280",
-            colorInputBackground: "#f3f4f6",
-          },
-        }}
+    <html lang="en" suppressHydrationWarning>
+      <body
+        suppressHydrationWarning
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
-        <html lang="en" suppressHydrationWarning>
-          <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
+        <ClerkProvider
+          appearance={{
+            variables: {
+              colorPrimary: "#e78a53",
+              colorBackground: "#f3f4f6",
+              colorText: "#111827",
+              colorTextSecondary: "#6b7280",
+              colorInputBackground: "#f3f4f6",
+            },
+          }}
+        >
+          <TanStackProvider>
             <UserSync />
             <Toaster />
             {children}
-          </body>
-        </html>
-      </ClerkProvider>
-    </TanStackProvider>
+          </TanStackProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
